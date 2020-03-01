869da827bd4433ef5a23550bcfde8d7a021af756
abdullo-davlatov <abdullotjk99@gmail.com>
2020-03-01 16:23:34 -0500

    created basic navigation class

---
+++ src/test/java/com/automation/tests/day2/JumpToTheNewWindow.java
@@ -0,0 +1,24 @@
+package com.automation.tests.day2;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class JumpToTheNewWindow {
+
+    public static void main(String[] args) throws Exception {
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://practice.cybertekschool.com/open_new_tab");
+
+        Thread.sleep(5000);
+
+        String windowHandle = driver.getWindowHandle();
+
+        System.out.println(windowHandle);
+
+    }
+
+}
