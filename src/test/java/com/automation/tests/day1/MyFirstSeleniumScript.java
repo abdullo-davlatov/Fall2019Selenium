2cfff580e8030a7f04a2d78c50cd852c61e58d14
abdullo-davlatov <abdullotjk99@gmail.com>
2020-03-01 12:32:59 -0500

    first commit

---
+++ src/test/java/com/automation/tests/day1/MyFirstSeleniumScript.java
@@ -0,0 +1,19 @@
+package com.automation.tests.day1;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyFirstSeleniumScript {
+
+    public static void main(String[] args) {
+        //setup chromedriver
+        WebDriverManager.chromedriver().setup();
+        //create chromedriver object
+        ChromeDriver driver = new ChromeDriver();
+        //open some website
+        driver.get("http://google.com");
+    }
+
+
+}
