9445fb8d1dd619044049c76df679443f8f94c5fa
abdullo-davlatov <abdullotjk99@gmail.com>
2020-03-10 20:48:03 -0400

    day 7: lacators

--- src/test/java/com/automation/tests/day7/Xpath.java
+++ src/test/java/com/automation/tests/day7/Xpath.java
@@ -2,15 +2,23 @@ package com.automation.tests.day7;
 
 import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.DriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class Xpath {
+    public static String userNameLocator = "//label[text()='Username']/following-sibling::input";
+    public static String passwordLocator = "//label[text()='Password']/following-sibling::input";
+    public static String loginBtnLocator = "//button[contains(text(), 'Login')]";
 
     public static void main(String[] args) {
-
         WebDriver driver = DriverFactory.createDriver("chrome");
         driver.get("http://practice.cybertekschool.com/login");
         BrowserUtils.wait(3);
+       // driver.findElement(By.xpath("//label[text()='Username']/following-sibling::input"));
+        driver.findElement(By.xpath(userNameLocator)).sendKeys("tomsmith");
+       // driver.findElement(By.xpath("//label[text()='Password']/following-sibling::input"));
+        driver.findElement(By.xpath(passwordLocator)).sendKeys("SuperSecretPassword");
+        driver.findElement(By.xpath(loginBtnLocator)).click();
 
 
         BrowserUtils.wait(3);
