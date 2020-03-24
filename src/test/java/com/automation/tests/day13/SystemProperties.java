2e96bc28062479937c21f4048ccf261a5bf75885
abdullo-davlatov <abdullotjk99@gmail.com>
2020-03-24 19:47:54 -0400

    static code

--- src/test/java/com/automation/tests/day13/SystemProperties.java
+++ src/test/java/com/automation/tests/day13/SystemProperties.java
@@ -7,6 +7,14 @@ public class SystemProperties {
     @Test
     public void test(){
         System.out.println(System.getProperty("user.dir"));
+        System.out.println(System.getProperty("os.name"));
+        System.out.println(System.getProperty("user.home"));
+
+        String pathToDownloads =  System.getProperty("user.home") + "\\Downloads";
+        System.out.println(pathToDownloads);
+
+        System.out.println(System.getProperty("os.arch"));
+
     }
 
 }
